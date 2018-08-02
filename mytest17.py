3cb6b03945bf5ca66277125c3f55ab35137b4684
piglite <piglite@vip.sina.com>
2018-08-02 19:09:47 +0800

    2018_8_2

--- mytest17.py
+++ mytest17.py
@@ -1,13 +1,5 @@
-from pymysql import *
-settings = {
-    "user":"root",
-    "host":"127.0.0.1",
-    "port":3306,
-    "password":"110101",
-    "database":"hfsql",
-    "charset":"utf8"
-}
-conn = connect(**settings)
-c = conn.cursor()
-c.execute('select * from boys')
-print(c.fetchall())
+
+
+
+
+
